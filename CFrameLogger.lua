d604688b2d0b961188e1f90170c06537830530bc
ProjectThasak <71513527+ProjectThasak@users.noreply.github.com>
2020-10-29 01:38:28 +0300

    Update CFrameLogger.lua

--- CFrameLogger.lua
+++ CFrameLogger.lua
@@ -5,10 +5,12 @@
 
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
-local TextBox = Instance.new("TextLabel")
-local TextButton = Instance.new("TextButton")
 local TextLabel = Instance.new("TextLabel")
-local cframe = Instance.new("TextButton")
+local Copy = Instance.new("TextButton")
+local Clear = Instance.new("TextButton")
+local label = Instance.new("TextLabel")
+local setcframe = Instance.new("TextButton")
+local animasyon = Instance.new("TextLabel")
 
 --Properties:
 
@@ -17,53 +19,174 @@ ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 Frame.Parent = ScreenGui
 Frame.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.308341146, 0, 0.330677271, 0)
-Frame.Size = UDim2.new(0, 409, 0, 142)
-
-TextBox.Parent = Frame
-TextBox.BackgroundColor3 = Color3.fromRGB(58, 58, 58)
-TextBox.BorderSizePixel = 0
-TextBox.Position = UDim2.new(0.0709046423, 0, 0.3859649, 0)
-TextBox.Size = UDim2.new(0, 349, 0, 25)
-TextBox.Font = Enum.Font.SourceSans
-TextBox.Text = ""
-TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextBox.TextSize = 14.000
-
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.Position = UDim2.new(0.511002421, 0, 0.70385474, 0)
-TextButton.Size = UDim2.new(0, 194, 0, 33)
-TextButton.Font = Enum.Font.Cartoon
-TextButton.Text = "Copy"
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.TextSize = 16.000
+Frame.Position = UDim2.new(0.266166836, 0, 0.254980087, 0)
+Frame.Size = UDim2.new(0, 500, 0, 245)
 
 TextLabel.Parent = Frame
 TextLabel.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
 TextLabel.BorderSizePixel = 0
-TextLabel.Size = UDim2.new(0, 409, 0, 25)
+TextLabel.Size = UDim2.new(0, 500, 0, 35)
 TextLabel.Font = Enum.Font.Cartoon
 TextLabel.Text = "CFrame Logger"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextSize = 16.000
 
-cframe.Name = "cframe"
-cframe.Parent = Frame
-cframe.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-cframe.Position = UDim2.new(0.017114915, 0, 0.70385474, 0)
-cframe.Size = UDim2.new(0, 194, 0, 33)
-cframe.Font = Enum.Font.Cartoon
-cframe.Text = "CFrame"
-cframe.TextColor3 = Color3.fromRGB(0, 0, 0)
-cframe.TextSize = 16.000
+Copy.Name = "Copy"
+Copy.Parent = Frame
+Copy.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
+Copy.BorderSizePixel = 0
+Copy.Position = UDim2.new(0, 0, 0.828571439, 0)
+Copy.Size = UDim2.new(0, 166, 0, 42)
+Copy.Font = Enum.Font.Cartoon
+Copy.Text = "Copy"
+Copy.TextColor3 = Color3.fromRGB(255, 255, 255)
+Copy.TextSize = 16.000
 
-local cf = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+Clear.Name = "Clear"
+Clear.Parent = Frame
+Clear.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
+Clear.BorderSizePixel = 0
+Clear.Position = UDim2.new(0.666304171, 0, 0.828571439, 0)
+Clear.Size = UDim2.new(0, 166, 0, 42)
+Clear.Font = Enum.Font.Cartoon
+Clear.Text = "Clear"
+Clear.TextColor3 = Color3.fromRGB(255, 255, 255)
+Clear.TextSize = 16.000
 
-cframe.MouseButton1Down:connect(function()
-	TextBox.Text = cf
-end)
+label.Name = "label"
+label.Parent = Frame
+label.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
+label.BorderSizePixel = 0
+label.Position = UDim2.new(0.0276497807, 0, 0.179591835, 0)
+label.Size = UDim2.new(0, 473, 0, 152)
+label.Font = Enum.Font.SourceSansLight
+label.Text = "--[[\\n    Made By Project#6666\\n    Press \"Insert\" to toggle GUI\\n    Are script currently broken. Please use \"F9\" to see CFrame\\n]]"
+label.TextColor3 = Color3.fromRGB(255, 255, 255)
+label.TextSize = 14.000
+label.TextXAlignment = Enum.TextXAlignment.Left
+label.TextYAlignment = Enum.TextYAlignment.Top
 
-TextButton.MouseButton1Down:connect(function()
-	setclipboard(TextBox.Text)
-end)
+setcframe.Name = "setcframe"
+setcframe.Parent = Frame
+setcframe.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
+setcframe.BorderSizePixel = 0
+setcframe.Position = UDim2.new(0.330304205, 0, 0.828571439, 0)
+setcframe.Size = UDim2.new(0, 168, 0, 42)
+setcframe.Font = Enum.Font.Cartoon
+setcframe.Text = "Set CFrame"
+setcframe.TextColor3 = Color3.fromRGB(255, 255, 255)
+setcframe.TextSize = 16.000
+
+animasyon.Name = "animasyon"
+animasyon.Parent = Frame
+animasyon.BackgroundColor3 = Color3.fromRGB(28, 28, 28)
+animasyon.BorderSizePixel = 0
+animasyon.Position = UDim2.new(-0.00200000009, 0, 1, 0)
+animasyon.Size = UDim2.new(0, 499, 0, 245)
+animasyon.Font = Enum.Font.SourceSans
+animasyon.Text = "GUI Loading. Please wait!"
+animasyon.TextColor3 = Color3.fromRGB(255, 255, 255)
+animasyon.TextSize = 24.000
+
+--[[
+    Made By Project#6666
+    Press "Insert" to toggle GUI
+    Are script currently broken. Please use "F9" to see CFrame
+]]
+
+-- Scripts:
+
+local function KWEUZM_fake_script() -- ScreenGui.LocalScript 
+	local script = Instance.new('LocalScript', ScreenGui)
+
+	local Frame = script.Parent.Frame
+	local copy = script.Parent.Frame.Copy
+	local clear = script.Parent.Frame.Clear
+	local label = script.Parent.Frame.label
+	local set = script.Parent.Frame.setcframe
+	local UIS = game:GetService("UserInputService")
+	local animasyon = script.Parent.Frame.animasyon
+	local textlabel = script.Parent.Frame.TextLabel
+	
+	animasyon.Visible = true
+	label.Visible = true
+	copy.Visible = true
+	set.Visible = true
+	clear.Visible = true
+	textlabel.Visible = true
+	
+	--[[
+	
+	size : 0, 499,0, 42
+	position : 0, 0,0.829, 0
+	]]
+	
+	animasyon.AnchorPoint = Vector2.new(0, 499,0, 245)
+	
+	animasyon.Text = "GUI Loading. Please wait!"
+	wait(1)
+	animasyon.Text = "GUI Loading.. Please wait!"
+	wait(1)
+	animasyon.Text = "GUI Loading... Please wait!"
+	wait(1)
+	animasyon.Text = "GUI Loading.... Please wait!"
+	wait(1)
+	animasyon.Text = "GUI Loading. Please wait!"
+	wait(1)
+	animasyon.Text = "GUI Loading.. Please wait!"
+	wait(1)
+	animasyon.Text = "GUI Loading... Please wait!"
+	wait(1)
+	animasyon.Text = "GUI Loading.... Please wait!"
+	wait(1)
+	animasyon.Text = ""
+	wait(1)
+	animasyon.Text = "GUI Loaded!"
+	wait(1)
+	animasyon.Text = ""
+	animasyon:TweenSize(UDim2.new(0, 499,0, 0))
+	wait(1)
+	animasyon.Visible = false
+	copy.Visible = true
+	clear.Visible = true
+	set.Visible = true
+	textlabel.Visible = true
+	
+	clear.MouseButton1Down:Connect(function()
+		print("We are currently disabled Clear button Please say (fix the script or u are gay) to Project#6666")
+		label.Text = "Press F9!"
+		wait(3)
+		label.Text = "--[[\\n    Made By Project#6666\\n    Press \"Insert\" to toggle GUI\\n    Are script currently broken. Please use \"F9\" to see CFrame\\n]]"
+	end)
+	
+	set.MouseButton1Down:Connect(function()
+		print(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame)
+	end)
+	
+	copy.MouseButton1Down:Connect(function()
+		print("We are currently disabled Copy button Please say (fix the script or u are gay) to Project#6666")
+		label.Text = "Press F9!"
+		wait(3)
+		label.Text = "--[[\\n    Made By Project#6666\\n    Press \"Insert\" to toggle GUI\\n    Are script currently broken. Please use \"F9\" to see CFrame\\n]]"
+	end)
+	
+	UIS.InputBegan:connect(function(k)
+		if k.KeyCode == Enum.KeyCode.Insert then
+			if Frame.Visible == true then
+				Frame.Visible = false
+			else
+				Frame.Visible = true
+			end
+		end
+	end)
+	
+	--[[
+	
+	kucuk sizeler : {0, 168},{0, 42}
+	
+	--[[
+	    Made By Project#6666
+	    Press "Insert" to toggle GUI
+	]]
+end
+coroutine.wrap(KWEUZM_fake_script)()
